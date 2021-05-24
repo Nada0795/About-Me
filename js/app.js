4a1c2a0b89731e00ad7eb616e4b0ada44c12df75
Nada0795 <nadakh1993@icloud.com>
2021-05-25 01:05:15 +0300

    add files

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
-//Questions 
-Do you want to learn a new skills?
-do you have the ability to work under pressure ? 
-Do you like programming??
-Do you want to be software developer?
-Are you ready to start with our course ?
-//
+// //Questions 
+// Do you want to learn a new skills?
+// do you have the ability to work under pressure ? 
+// Do you like programming??
+// Do you want to be software developer?
+// Are you ready to start with our course ?
+// //
+
+
+if (1=23.dsdsewr){
+
+
+}
