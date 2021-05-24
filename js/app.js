febe094a4bde212d48999e416a6538a4aeb3f295
Nada0795 <nadakh1993@icloud.com>
2021-05-24 22:24:07 +0300

    add my BIO file to organaized my project

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//Questions 
+Do you want to learn a new skills?
+do you have the ability to work under pressure ? 
+Do you like programming??
+Do you want to be software developer?
+Are you ready to start with our course ?
+//
